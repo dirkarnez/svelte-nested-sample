9268e80472b338e9710a7b2f95bf1954a6d80b73
Dirk Arnez <smalldirkalex@gmail.com>
2023-10-19 17:26:40 +0800

    - add Nested.svelte

---
+++ Nested.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Nested from "./Nested.svelte";
+  export let depth = 0;
+</script>
+
+
+<div class="recursive-component">
+  <p>Depth: {depth}</p>
+  
+  {#if depth < 5}
+    <Nested depth={depth + 1}>
+      <p>Custom content for depth {depth + 1}</p>
+    </Nested>
+  {/if}
+</div>
+
+<style>
+  .recursive-component {
+    margin-left: 10px;
+    padding: 10px;
+    border: 1px solid #ccc;
+  }
+</style>
